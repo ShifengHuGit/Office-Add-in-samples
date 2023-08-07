4e60742552b9cb24778158866ca3a5bf776966d8
Shifeng Hu <Shifeng.h@gmail.com>
2023-08-07 15:19:21 +0800

    Update taskpane.js
    dfg

--- Excel-custom-functions/Batching/src/taskpane/taskpane.js
+++ Excel-custom-functions/Batching/src/taskpane/taskpane.js
@@ -6,7 +6,7 @@ Office.initialize = () => {
   document.getElementById('sideload-msg').style.display='none';
   document.getElementById('app-body').style.display='flex';
   document.getElementById('run').onclick = run;
-  document.getElementById("getCellData").onclick = showSelection;
+  document.getElementById("getCellData").onclick = getData;
 };
 
 async function run() {
